da29d6f090662cc6291f091c7baa43496b462d06
mydeveloperplanet <gunter@mydeveloperplanet.com>
2023-01-14 17:21:13 +0100

    Initial commit

---
+++ backend/src/main/java/com/mydeveloperplanet/mysbomplanet/MySbomPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.mysbomplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySbomPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySbomPlanetApplication.class, args);
+	}
+
+}
